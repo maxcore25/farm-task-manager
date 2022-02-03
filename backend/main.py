15606156553d8e1c68c7a37cc8b1c4a4d536e7c9
maxcore25 <mkorobov2002@mail.ru>
2022-02-03 09:03:07 +0300

    FastAPI CRUD Methods done

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
-
-app = FastAPI()
-
 from database import (
     fetch_one_todo,
     fetch_all_todos,
@@ -11,6 +8,9 @@ from database import (
     update_todo,
     remove_todo
 )
+from model import Todo
+
+app = FastAPI()
 
 origins = ['https://localhost:3000']
 
@@ -30,27 +30,40 @@ def read_root():
 
 @app.get("/api/todo")
 async def get_todo():
-    return 1
+    response = await fetch_all_todos()
+    return response
 
 
-@app.get("/api/todo{id}")
-async def get_todo_by_id(id):
-    return 1
+@app.get("/api/todo{title}", response_model=Todo)
+async def get_todo_by_id(title):
+    response = await fetch_one_todo(title)
+    if response:
+        return response
+    raise HTTPException(404, f'There is no TODO item with this title: {title}')
 
 
-@app.post("/api/todo")
-async def post_todo(todo):
-    return 1
+@app.post("/api/todo", response_model=Todo)
+async def post_todo(todo: Todo):
+    response = await create_todo(todo.dict())
+    if response:
+        return response
+    raise HTTPException(400, 'Something went wrong / Bad Request')
 
 
-@app.put("/api/todo{id}")
-async def put_todo(id, data):
-    return 1
+@app.put("/api/todo{title}", response_model=Todo)
+async def put_todo(title: str, desc: str):
+    response = await update_todo(title, desc)
+    if response:
+        return response
+    raise HTTPException(404, f'There is no TODO item with this title: {title}')
 
 
-@app.delete("/api/todo{id}")
-async def delete_todo(id):
-    return 1
+@app.delete("/api/todo{title}", response_model=Todo)
+async def delete_todo(title):
+    response = await remove_todo(title)
+    if response:
+        return f'TODO item with title "{title}" removed'
+    raise HTTPException(404, f'There is no TODO item with this title: {title}')
 
 
 if __name__ == '__main__':
