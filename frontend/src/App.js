80e20e0e414f1324df1ffb39bf08c404d460b8fc
maxcore25 <mkorobov2002@mail.ru>
2022-02-04 09:47:38 +0300

    Bootstrap connected and Basic styling started

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div
+        className='App list-group-item justify-content-center align-items-center mx-auto'
+        style={{ width: 400, backgroundColor: 'white', marginTop: 15 }}>
+        <h1
+          className='card text-white bg-primary mb-1'
+          styleName='max-width: 20rem;'>
+          Task Manager
+        </h1>
+      </div>
     </div>
   );
 }
